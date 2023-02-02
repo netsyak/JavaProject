fb5c86aeb8406d289ddea3d05eb329ff61203ded
Kwang-Yeon Kim <117058050+netsyak@users.noreply.github.com>
2023-02-03 00:32:03 +0900

    Update MemberVo.java

--- javapro/MemberVo.java
+++ javapro/MemberVo.java
@@ -1,25 +1,50 @@
-package javapro;
+package dao;
 
 public class MemberVo {
-	private String id; // 수정
+	private String id;
 	private String password; // 수정
-//	private int sal;
+//	private String pwd;
+	private String name;
+	private String birth;
+	private String tel;
+	private String address;
 
-	public MemberVo(String id, String password) {
+//	public MemberVo(String id, String password) {
+//	public MemberVo(String id, String pwd ) {
+	public MemberVo(String id, String password, String name, String birth, String tel, String address) {
 		this.id = id;
 		this.password = password;
-//		this.sal = sal;
+//		this.pwd = pwd;
+		this.name = name;
+		this.birth = birth;
+		this.tel = tel;
+		this.address = address;
+
 	}
 
 	public String getId() {
 		return id;
 	}
 
-	public String getPassword() {
+	public String getPwd() {
 		return password;
+//		return pwd;
 	}
 
-//	public int getSal() {
-//		return sal;
+	public String getName() {
+		return name;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public String getAddress() {
+		return address;
+	}
 
 }
